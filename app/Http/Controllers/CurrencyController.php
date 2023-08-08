ee502a5419fadc96cf9e083979912c875e6f53e2
Skogkattt <wayne79687968@gmail.com>
2023-08-09 00:11:22 +0800

    Feat: add CurrencyController and exchange api route

---
+++ app/Http/Controllers/CurrencyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class CurrencyController extends BaseController
+{
+    public function exchange()
+    {
+    }
+}
