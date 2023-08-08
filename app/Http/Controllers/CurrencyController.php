108149f01655fd39338e8e57d8266544275dd3dd
Skogkattt <wayne79687968@gmail.com>
2023-08-09 00:35:32 +0800

    Feat: CurrencyController exchange function

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -3,10 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class CurrencyController extends BaseController
 {
-    public function exchange()
+    protected $rates = [
+        "TWD" => [
+            "TWD" => 1,
+            "JPY" => 3.669,
+            "USD" => 0.03281
+        ],
+        "JPY" => [
+            "TWD" => 0.26956,
+            "JPY" => 1,
+            "USD" => 0.00885
+        ],
+        "USD" => [
+            "TWD" => 30.444,
+            "JPY" => 111.801,
+            "USD" => 1
+        ]
+    ];
+
+    public function exchange(Request $request)
     {
+        $amount = str_replace(['$', ','], '', $request->input('amount'));
+        $amount = floatval($amount);
+        $source = $request->input("source");
+        $target = $request->input("target");
+
+        $rates = $this->rates;
+        $rate = floatval($rates[$source][$target]);
+        $convertedAmount = $amount * $rate;
+
+        $convertedAmount = number_format(round($convertedAmount, 2), 2);
+
+        return response()->json(["result" => "success", "data" => ["amount" => "$" . $convertedAmount]]);
     }
 }
