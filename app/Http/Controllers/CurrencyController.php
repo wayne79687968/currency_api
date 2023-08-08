3ef01add21f5180cbead2cb9c33f69bcce540202
Skogkattt <wayne79687968@gmail.com>
2023-08-09 01:05:55 +0800

    Feat: add exchangerate api

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CurrencyController extends BaseController
 {
@@ -49,5 +50,44 @@ class CurrencyController extends BaseController
         $convertedAmount = number_format(round($convertedAmount, 2), 2);
 
         return response()->json(["result" => "success", "data" => ["amount" => "$" . $convertedAmount]]);
+    }
+
+    public function exchangeByExchangerateApi(Request $request)
+    {
+        if (!$request->has(["amount", "source", "target"])) {
+            return response()->json(["result" => "error", "msg" => "Missing parameters"], 400);
+        }
+
+        $amount = str_replace(["$", ","], "", $request->input("amount"));
+        $amount = floatval($amount);
+        $source = $request->input("source");
+        $target = $request->input("target");
+
+        $apiKey = env("EXCHANGE_RATE_API_KEY");
+
+        $url = "https://v6.exchangerate-api.com/v6/{$apiKey}/latest/{$source}";
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode != 200) {
+            Log::error("Exchangerate-api Error, url: {$url}, err_msg: {$response}");
+            return response()->json(["result" => "error", "msg" => "Exchangerate-api Error, url: {$url}, err_msg: {$response}"], 400);
+        }
+
+        $data = json_decode($response, true);
+
+        if (!isset($data["conversion_rates"][$target])) {
+            Log::error("no $target in conversion_rates");
+            return response()->json(["result" => "error", "msg" => "no $target in conversion_rates"], 400);
+        }
+
+        $rate = floatval($data["conversion_rates"][$target]);
+        $convertedAmount = $amount * $rate;
+        $convertedAmount = number_format(round($convertedAmount, 2), 2);
+
+        return response()->json(["msg" => "success", "data" => ["amount" => "$" . $convertedAmount]]);
     }
 }
